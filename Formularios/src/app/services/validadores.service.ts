d95c0c89a62a48a5f1f10da8202fa78e53063160
PrimoMax <mfnunez36@gmail.com>
2020-08-10 21:06:43 -0400

    Ultimos cambios se agregan validadores asincronos

--- Formularios/src/app/services/validadores.service.ts
+++ Formularios/src/app/services/validadores.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+interface IErrorValidate {
+  [s: string]: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +13,53 @@ export class ValidadoresService {
 
   constructor() { }
 
+  validaPassword(control: FormControl): {[ s: string ]: boolean} {
 
+    if ( control.value.length <= 5) {
+      return {
+        passwordInvalid: true
+      };
+    }
+
+    return null;
+  }
+
+  validaEmails(control: FormControl): {[ s: string ]: boolean} {
+    const regexEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+
+    if ( regexEmail.test(control.value) ) {
+      return {
+        emailinvalid: true
+      };
+    }
+
+    return null;
+  }
+
+  validaPasswordsIguales(pass: string, pass2: string) {
+
+    return (formGroup: FormGroup) => {
+
+      const pass1ctrl = formGroup.controls[pass];
+      const pass2ctrl = formGroup.controls[pass2];
+
+      if ( pass1ctrl === pass2ctrl ) {
+
+      } else {
+        pass2ctrl.setErrors({ noEsIgual: true });
+      }
+    };
+  }
+
+  existeUser(): Promise<IErrorValidate> | Observable<IErrorValidate> {
+    return new Promise( (resolve, reject) => {
+
+      setTimeout( () => {
+        if () {
+
+        }
+      }, 3000);
+    });
+  }
 
 }
