ade56573e7633e129186dd9542cf95173501d4c1
Max <mfnunez36@gmail.com>
2020-11-04 02:34:40 -0300

    MiniBanco

--- MiniBanco/server/server/config/database.js
+++
@@ -1,7 +0,0 @@
-let user = 'sharapp';
-let pass = 'Ix0gSXrRBSDB24AN';
-let dbname= 'ripley';
-
-module.exports = {
-    'url': `mongodb+srv://${ user }:${ pass }@cluster0.peksg.mongodb.net/${ dbname }?retryWrites=true&w=majority`
-}
